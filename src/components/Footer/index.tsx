bb2e3321fc51feba0623e60de5bcba140fb0b07d
jcgonzaga <gonzaga.jc1993@gmail.com>
2020-11-23 15:57:08 +0800

    Made Footer component dynamic

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,21 +1,18 @@
 import React from "react";
 import assets from "assets";
+import { footerDetails } from "constants/footer";
 import styles from "./Footer.scss";
 
 const Footer: React.FC = () => (
   <div className={styles.wrapper}>
     <div className={styles.imgContainer}>
-      <a href={"https://github.com/JCGonzaga01"} target={"_blank"}>
-        <img src={assets.github} alt={"Github"} />
-      </a>
-      <a href={"https://www.linkedin.com/in/jc-gonzaga/"} target={"_blank"}>
-        <img src={assets.linkedin} alt={"LinkedIn"} />
-      </a>
-      <a href={"mailto:gonzaga.jc1993@gmail.com"} target={"_blank"}>
-        <img src={assets.email} alt={"Email"} />
-      </a>
+      {footerDetails.linkItems.map((item, idx) => (
+        <a key={`${idx}=${item.name}`} href={item.link} target={item.target}>
+          <img src={item.icon} alt={item.name} />
+        </a>
+      ))}
     </div>
-    <div className={styles.myName}>© John Christopher Gonzaga</div>
+    <div className={styles.myName}>{footerDetails.name}</div>
   </div>
 );
 
