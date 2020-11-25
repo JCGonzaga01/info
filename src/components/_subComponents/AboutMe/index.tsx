a67d816fcaf7b40101fb0ee1274287a8e18dca56
jcgonzaga <gonzaga.jc1993@gmail.com>
2020-11-25 22:43:01 +0800

    Fixed AboutMe Flexibility

--- src/components/_subComponents/AboutMe/index.tsx
+++ src/components/_subComponents/AboutMe/index.tsx
@@ -1,22 +1,19 @@
 import React from "react";
 import { List } from "components/_common";
 import { aboutMeItems } from "constants/aboutMe";
+import { classNames } from "helpers/functions";
 import styles from "./AboutMe.scss";
 
 const AboutMe: React.FC = () => {
   return (
-    <div className={styles.fadeIn}>
-      {aboutMeItems.map((itemRow, idx) => (
-        <div key={idx} className={styles.wrapper}>
-          {itemRow.map((item, idx) => (
-            <div key={`${idx}-${item.title}`} className={styles.itemDetails}>
-              <div>
-                <img src={item.icon} alt={item.title} />
-                <div className={styles.title}>{item.title}</div>
-                <List items={item.details} isEllipted minItems={3} />
-              </div>
-            </div>
-          ))}
+    <div className={classNames(styles.wrapper, styles.fadeIn)}>
+      {aboutMeItems.map((item, idx) => (
+        <div key={`${idx}-${item.title}`} className={styles.itemDetails}>
+          <div>
+            <img src={item.icon} alt={item.title} />
+            <div className={styles.title}>{item.title}</div>
+            <List items={item.details} isEllipted minItems={3} />
+          </div>
         </div>
       ))}
     </div>
--- src/constants/aboutMe.ts
+++ src/constants/aboutMe.ts
@@ -1,59 +1,55 @@
 import assets from "assets";
 
 export const aboutMeItems = [
-  [
-    {
-      icon: assets.like,
-      title: "I Like",
-      details: [
-        "Biking",
-        "Jogging",
-        "Playing Guitar",
-        "Playing Cajon",
-        "Playing Keyboard (Still learning :D )",
-        "Mountain Hiking",
-        "Listening to christian music",
-        "Listening to christian podcast",
-        "Joining christian church activites",
-        "and Going to an open field like parks and lie under a tree in a cloudy day while listening to my favorite music. ",
-      ],
-    },
-    {
-      icon: assets.dislike,
-      title: "I don't like",
-      details: [
-        "Beer",
-        "Coffee",
-        "Onions in my salad",
-        "Bell pepper even nicely cook",
-        "Swimming and/or getting stuck in the middle of the ocean (Thalassophobia)",
-      ],
-    },
-  ],
-  [
-    {
-      icon: assets.funFact,
-      title: "Fun Facts",
-      details: [
-        "When I was in Japan, I was stuck and can't find the exit for almost 30mins in Shinjuku Station, Japan's Largest Railway Station, ",
-        "I was bitten by a dog with rabbies when I was 7 or 8 yrs. old. Luckily, I survive.",
-        "When I was young, my friends and I almost burned down a field and our houses because of fireworks.",
-        "We also almost got blind when we try to experiment with firework's powder and then it suddenly light up.",
-        "I enjoy watching people's everyday life in one of Japan's busiest prefacture, Kawasaki.",
-        "I still drink milk every morning and sometimes before going to bed.",
-      ],
-    },
-    {
-      icon: assets.bucketList,
-      title: "Bucket Lists",
-      details: [
-        "Visit Switzerland to see Swiss Alps, ride Glacier Express in winter and go to Jungfraujoch - Top of Europe in winter as well.",
-        "See Aurora Borealis either in Norway or in Iceland.",
-        "Experience a Business-Class and First-Class Flight Seat.",
-        "Climb Mt.Fuji in Japan.",
-        "Learn how to skii or snowboard.",
-        "I have many more but can't share all of them here. :) ",
-      ],
-    },
-  ],
+  {
+    icon: assets.like,
+    title: "I Like",
+    details: [
+      "Biking",
+      "Jogging",
+      "Playing Guitar",
+      "Playing Cajon",
+      "Playing Keyboard (Still learning :D )",
+      "Mountain Hiking",
+      "Listening to christian music",
+      "Listening to christian podcast",
+      "Joining christian church activites",
+      "and Going to an open field like parks and lie under a tree in a cloudy day while listening to my favorite music. ",
+    ],
+  },
+  {
+    icon: assets.dislike,
+    title: "I don't like",
+    details: [
+      "Beer",
+      "Coffee",
+      "Onions in my salad",
+      "Bell pepper even nicely cook",
+      "Swimming and/or getting stuck in the middle of the ocean (Thalassophobia)",
+    ],
+  },
+  {
+    icon: assets.funFact,
+    title: "Fun Facts",
+    details: [
+      "When I was in Japan, I was stuck and can't find the exit for almost 30mins in Shinjuku Station, Japan's Largest Railway Station, ",
+      "I was bitten by a dog with rabbies when I was 7 or 8 yrs. old. Luckily, I survive.",
+      "When I was young, my friends and I almost burned down a field and our houses because of fireworks.",
+      "We also almost got blind when we try to experiment with firework's powder and then it suddenly light up.",
+      "I enjoy watching people's everyday life in one of Japan's busiest prefacture, Kawasaki.",
+      "I still drink milk every morning and sometimes before going to bed.",
+    ],
+  },
+  {
+    icon: assets.bucketList,
+    title: "Bucket Lists",
+    details: [
+      "Visit Switzerland to see Swiss Alps, ride Glacier Express in winter and go to Jungfraujoch - Top of Europe in winter as well.",
+      "See Aurora Borealis either in Norway or in Iceland.",
+      "Experience a Business-Class and First-Class Flight Seat.",
+      "Climb Mt.Fuji in Japan.",
+      "Learn how to skii or snowboard.",
+      "I have many more but can't share all of them here. :) ",
+    ],
+  },
 ];
